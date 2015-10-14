89ae57d5f1bbcda1f7441c61461c80be553772b2
austinalig <austinalig@knights.ucf.edu>
2015-10-14 12:24:27 -0700

    Added Camera Toggle and Pathfinding
    ^ Camera toggle with "P" to switch perspective from 1st to 3rd person,
    "C" for crouching but character crouch needs to be put on a toggle as
    well. All NPCs  have pathfinding using Navmesh. Currently, all NPCs
    pathfind to the immediate right of the player character (can path
    anywhere based on a transform).

---
+++ Space/Assets/CameraToggle.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraToggle : MonoBehaviour {
+	private Transform cam;
+	private bool flag;
+	private bool cflag;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Transform> ();
+		flag = true;
+		cflag = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKeyDown (KeyCode.P))
+		{//On "P" toggle the camera view by shifting the transform
+			if (flag)
+			{
+				cam.position = (cam.position + (cam.rotation * new Vector3(0, 0, 3)));
+				flag = false;
+			}
+			else 
+			{
+				cam.position = (cam.position + (cam.rotation * new Vector3(0, 0 ,-3)));
+				flag = true;
+			}
+	} 
+		if(Input.GetKeyDown (KeyCode.C))
+		{//On "P" toggle the camera view by shifting the transform
+			if (cflag)
+			{
+				cam.position = (cam.position + new Vector3(0 , -1, 0));
+				cflag = false;
+			}
+			else 
+			{
+				cam.position = (cam.position + new Vector3(0 , 1, 0));
+				cflag = true;
+			}
+	}
+}
+}
--- Space/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Space/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -8,12 +8,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     public class ThirdPersonUserControl : MonoBehaviour
     {
         private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+        public Transform m_Cam;                  // A reference to the main camera in the scenes transform
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+		private bool flag;
 
-        
         private void Start()
         {
             // get the transform of the main camera
@@ -48,7 +48,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+            bool crouch = Input.GetKeyDown(KeyCode.C);
 
             // calculate move direction to pass to character
             if (m_Cam != null)
