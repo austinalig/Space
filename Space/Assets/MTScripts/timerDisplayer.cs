c4294acef17b91f6f49eb9e9bcc0969c61efd57f
Montrel <trel100@yahoo.com>
2015-10-16 11:41:48 -0400

    Everything So Far

---
+++ Space/Assets/MTScripts/timerDisplayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timerDisplayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Mathf.Round(Time.time) % 5 == 0)
+        {
+            Debug.Log(Mathf.Round(Time.time));
+
+        }
+	
+	}
+}
