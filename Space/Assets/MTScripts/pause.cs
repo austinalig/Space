d9f77d3c14b61f656029490f811cc6a9938c924f
Montrel <trel100@yahoo.com>
2016-01-15 20:06:55 -0500

    Pause Added
    Added a script that allows the game to be paused. Easy enough to
    implement, but it makes models disappear.

---
+++ Space/Assets/MTScripts/pause.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pause : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
---
+++ Space/Assets/MTScripts/spawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+    public GameObject objectToSpawn;
+    public int numberOfSpawns = 0;
+    public Transform targetLocation;
+    private GameObject [] clonesOfObject;
+    private int spawnCount;
+    //private float xPos;
+   // private float zPos;
+	// Use this for initialization
+	void Start () {
+        clonesOfObject = new GameObject[numberOfSpawns];
+       // clonesOfObject[0] = GameObject.Instantiate(objectToSpawn);
+      //  xPos = targetLocation.position.x;
+      //  zPos = targetLocation.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(spawnCount < numberOfSpawns)
+        {
+            GameObject.Instantiate(objectToSpawn);
+           // clonesOfObject[spawnCount].trasnform = new Vector3()
+            spawnCount++;
+        }
+
+	}
+}
