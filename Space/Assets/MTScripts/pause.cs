e6372ed37b900a98a83b36c82466a74dcd3bf749
Montrel <trel100@yahoo.com>
2016-01-17 11:57:00 -0500

    Pause Screen Added to Button
    Press P to pause game.

--- Space/Assets/MTScripts/pause.cs
+++ Space/Assets/MTScripts/pause.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class pause : MonoBehaviour
 {
 
+    private GameObject pauseText;
+
     // Use this for initialization
     void Start()
     {
+        pauseText = GameObject.Find("PauseText");
     }
 
     // Update is called once per frame
@@ -17,9 +20,11 @@ public class pause : MonoBehaviour
             if (Time.timeScale == 1)
             {
                 Time.timeScale = 0;
+                pauseText.GetComponent<SpriteRenderer>().enabled = true;
             }
             else
             {
+                pauseText.GetComponent<SpriteRenderer>().enabled = false;
                 Time.timeScale = 1;
             }
         }
