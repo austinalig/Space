e9dbd67b3d1ab803f1600628693701a8847a44fa
Montrel <trel100@yahoo.com>
2016-01-17 21:25:04 -0500

    Basic AI Vision
    AI can now see objects. If you step into his field of view, he response
    on the debug window.

---
+++ Space/Assets/MTScripts/AIVision.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIVision : MonoBehaviour {
+
+    private ArrayList visible;
+
+	// Use this for initialization
+	void Start () {
+        visible = new ArrayList();
+        Debug.Log("Vision Active");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider visibleObject)
+    {
+        visible.Add(visibleObject.gameObject);
+
+       // if (visibleObject.gameObject.name.Equals("ThirdPersonController"))
+      //  {
+            Debug.Log("I see you");
+      //  }
+
+    }
+
+    void OnTriggerExit(Collider visibleObject)
+    {
+        visible.Remove(visibleObject.gameObject);
+
+        if (visibleObject.gameObject.name.Equals("ThirdPersonController"))
+        {
+            Debug.Log("I lost you");
+        }
+
+    }
+}
