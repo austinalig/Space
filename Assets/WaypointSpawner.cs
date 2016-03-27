81b7abc918d0ffd2ca5a1405067c549bc27ff3ca
Jimmy Lam <jimmylam621@gmail.com>
2016-03-27 15:16:20 -0400

    edits to scripts
    made it so that enemies only spawn ten at a time, and set their target
    to first person character

--- Assets/WaypointSpawner.cs
+++ Assets/WaypointSpawner.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WaypointSpawner : MonoBehaviour {
-		public Rigidbody projectile;
-		public int speed = 0;
-		public float fireRate = 0.5F;
-		private float nextFire = 0.0F;
-		void Update() {
-			if (Time.time > nextFire) {
-				nextFire = Time.time + fireRate;
-				Rigidbody clone = Instantiate(projectile, transform.position, transform.rotation)as Rigidbody;
-				if(clone != null){
-					clone.velocity = transform.TransformDirection(Vector3.forward * speed);}
-			}
-		}
-	}
+public class WaypointSpawner : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public int speed = 0;
+    public float fireRate = 0.5F;
+    private float nextFire = 0.0F;
+
+    public GameObject[] enemyCount;
+
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (enemyCount.Length <= 10)
+        {
+            if (Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                Rigidbody clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+                if (clone != null)
+                {
+                    clone.velocity = transform.TransformDirection(Vector3.forward * speed);
+                }
+            }
+        }
+    }
+}
