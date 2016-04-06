0234087f79fecf198b539082233a6280b0dd0e45
austinalig <austinalig@knights.ucf.edu>
2016-04-06 16:01:09 -0400

    Fixed Gm
    Fixed GM

--- 3DShooter/Assets/Gridmap.cs
+++ 3DShooter/Assets/Gridmap.cs
@@ -17,22 +17,20 @@ public class Gridmap : MonoBehaviour
         int i = 0, rand1, rand2;
         while (i < 1)
         {
-            Debug.Log("%");
             rand1 = Random.Range(4, 11);
             rand2 = Random.Range(4, 11);
             map[i] = new int[rand1, rand2];
-            Fillnext(map[i], 0, 0, rand1, rand2);
+            map[1] = Fillnext(map[i], 0, 0, rand1, rand2);
             BuildMap(map[i], cubesize, rand1, rand2);
             i++;
         }
     }
 
-    void Fillnext(int[,] map, int x, int y, int XMAX, int YMAX)
+    int[,] Fillnext(int[,] map, int x, int y, int XMAX, int YMAX)
     {
-        int rand1 = Random.Range(0, 1), rand2 = Random.Range(0, 1), rand3 = Random.Range(0, 1), rand4 = Random.Range(0, 1);
+        int rand1 = Random.Range(0, 2), rand2 = Random.Range(0, 2), rand3 = Random.Range(0, 2), rand4 = Random.Range(0, 2);
         int counter = 0;
         ///Decide how to generate Curr. Using rng, or deterministically
-        int curr = map[x, y];
         if (x == 0)
             counter = counter;
         else if (((map[x - 1, y] / 2) % 2 == 1) || ((map[x - 1, y] == 0) && (rand1 == 1)))
@@ -50,6 +48,7 @@ public class Gridmap : MonoBehaviour
         else if ((map[x, y + 1] % 8 / 4 == 1) || ((map[x, y + 1] == 0) && (rand4 == 1)))
             counter += 1;
         map[x, y] = counter;
+        int curr = map[x, y];
 
         if (curr % 2 == 1)//down
         {
@@ -74,20 +73,20 @@ public class Gridmap : MonoBehaviour
             if (map[x + 1, y] == 0)
                 Fillnext(map, x + 1, y, XMAX, YMAX);
         }
-        return;
+        return map;
     }
 
     void BuildMap(int[,] map, int scale, int XDIM, int YDIM)
     {
 
         int i, j, v;
-        GameObject level;
+        GameObject level = level6;
         int rot = 0;
-
-
-        for (i = 0; i < XDIM; i++)
+        Debug.Log(XDIM);
+        Debug.Log(YDIM);
+        for (i = 0; i < XDIM - 1; i++)
         {
-            for (j = 0; j < YDIM; i++)
+            for (j = 0; j < YDIM; j++)
             {
                 v = map[i, j];
                 switch (v)
@@ -172,7 +171,7 @@ public class Gridmap : MonoBehaviour
                         rot = 90;
                         break;
                 }
-                Instantiate(level, new Vector3(i * scale, j * scale, 0), Quaternion.Euler(0, rot, 0));
+                Instantiate(level, new Vector3(i * scale, 0, -j * scale), Quaternion.Euler(0, rot, 0));
 
 
             }
